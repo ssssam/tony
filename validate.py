068b09fb5fdfbdf2b754f5143b499efc5cce091b
Sam Thursfield <sam.thursfield@codethink.co.uk>
2012-07-11 19:12:31 +0100

    validate.py: Make more useful generally

--- validate.py
+++ validate.py
@@ -3,10 +3,29 @@
 import glob
 import json
 
+errors = False
+
 for morphology in glob.glob('*.morph'):
 	f = open (morphology)
 	try:
-		json.load (f)
-	except Exception:
-		print "%s is bad" % morphology
+		data = json.load (f)
+	except ValueError as e:
+		print "Error in %s: %s" % (morphology, e)
+		errors = True
+		continue
 
+	chunk_list = []
+
+	if 'sources' not in data:
+		# System, presumably
+		continue
+
+	for chunk in data['sources']:
+		chunk_list.append (chunk['name'])
+
+		for build_dep in chunk['build-depends']:
+			if build_dep not in chunk_list:
+				errors = True
+				print "%s: unknown build-dep %s" % (chunk['name'], build_dep)
+
+exit (errors)
